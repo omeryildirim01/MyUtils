86248668c5a5f573d613424edf1381472b33b44e
uin3566 <uin3566.gmail.com>
2017-12-16 15:39:29 +0800

    ObjectReleaseBeforeInitManager use single thread

--- app/src/main/java/com/xufang/myutils/utils/ObjectReleaseBeforeInitManager.java
+++ app/src/main/java/com/xufang/myutils/utils/ObjectReleaseBeforeInitManager.java
@@ -6,6 +6,7 @@ import java.util.Map;
 /**
  * Created by xufang on 2017/9/18.
  * 当创建新对象时，保证新对象的onInit在旧对象的onRelease之后调用。onInit,onRelease由业务实现
+ * 注：不支持多线程！！！
  * <p>
  * 栗子：当Activity持有一个单例时，如果新Activity的onCreate在旧Activity的onDestroy之前调用，
  * 而这个单例在onCreate中初始化并已开始使用，那么旧Activity调用onDestroy时会reset这个单例，就可能会出问题。
@@ -15,15 +16,9 @@ public final class ObjectReleaseBeforeInitManager {
     private static final String TAG = "ObjectReleaseBeforeInitManager";
 
     private Map<String, ReleaseBeforeInitObject> mObjectMap;
-    private Map<String, byte[]> mLockMap;
 
     private ObjectReleaseBeforeInitManager() {
         mObjectMap = new HashMap<>();
-        mLockMap = new HashMap<>();
-    }
-
-    private static class Holder {
-        private static final ObjectReleaseBeforeInitManager INSTANCE = new ObjectReleaseBeforeInitManager();
     }
 
     public static ObjectReleaseBeforeInitManager getInstance() {
@@ -36,16 +31,13 @@ public final class ObjectReleaseBeforeInitManager {
         }
 
         String type = target.getType();
-        putLockToMapByType(type);
 
-        synchronized (mLockMap.get(type)) {
-            ReleaseBeforeInitObject releaseBeforeInitObject = mObjectMap.get(type);
-            if (releaseBeforeInitObject != null) {
-                releaseBeforeInitObject.onRelease();
-            }
-            mObjectMap.put(type, target);
-            target.onInit();
+        ReleaseBeforeInitObject releaseBeforeInitObject = mObjectMap.get(type);
+        if (releaseBeforeInitObject != null) {
+            releaseBeforeInitObject.onRelease();
         }
+        mObjectMap.put(type, target);
+        target.onInit();
     }
 
     public void callRelease(ReleaseBeforeInitObject target) {
@@ -54,26 +46,11 @@ public final class ObjectReleaseBeforeInitManager {
         }
 
         String type = target.getType();
-        putLockToMapByType(type);
 
-        synchronized (mLockMap.get(type)) {
-            ReleaseBeforeInitObject releaseBeforeInitObject = mObjectMap.get(type);
-            if (releaseBeforeInitObject != null && releaseBeforeInitObject.equals(target)) {
-                releaseBeforeInitObject.onRelease();
-                mObjectMap.remove(type);
-            }
-        }
-    }
-
-    private void putLockToMapByType(String type) {
-        byte[] lock = mLockMap.get(type);
-        if (lock == null) {
-            synchronized (this) {
-                if (!mLockMap.containsKey(type)) {
-                    lock = new byte[0];
-                    mLockMap.put(type, lock);
-                }
-            }
+        ReleaseBeforeInitObject releaseBeforeInitObject = mObjectMap.get(type);
+        if (releaseBeforeInitObject != null) {
+            releaseBeforeInitObject.onRelease();
+            mObjectMap.remove(type);
         }
     }
 
@@ -83,5 +60,9 @@ public final class ObjectReleaseBeforeInitManager {
         void onRelease();
 
         String getType();
+    }
+
+    private static class Holder {
+        private static final ObjectReleaseBeforeInitManager INSTANCE = new ObjectReleaseBeforeInitManager();
     }
 }
